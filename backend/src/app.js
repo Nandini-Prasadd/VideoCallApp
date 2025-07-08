c94d25d3876bb789bf7682724223da3219fb2c10
nandini-prasadd <nandini070418@gmail.com>
2025-07-08 18:58:42 +0530

    cors error sort

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,16 @@ const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+// app.options("*", cors());
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ extended: true, limit: "40kb" }));
 
