581694e930cf4d24f1535e9ad58d41d4da4d2133
nandini-prasadd <nandini070418@gmail.com>
2025-07-09 16:51:17 +0530

    changes 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,18 +21,12 @@ app.use(
   cors({
     origin: "https://zapchatfrontend-syf7.onrender.com",
     credentials: true,
-  })
-);
-
-app.options(
-  "*",
-  cors({
-    origin: "https://zapchatfrontend-syf7.onrender.com",
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "40kb" }));
